835f1df8af8b0edecae3e5ee2759bf06f4f32118
rickslimmen <rickslimmen@live.nl>
2015-10-06 17:52:16 +0200

    WolfjesBewegen

--- model/Enemy_Following.cs
+++ model/Enemy_Following.cs
@@ -30,11 +30,13 @@ namespace KBS1.model
             if(playerX > Position_X)
             {
                 Position_X += Speed_X;
+                Direction = ObjectDirection.RIGHT;
             }
             //Left
             else if(playerX < Position_X)
             {
                 Position_X -= Speed_X;
+                Direction = ObjectDirection.LEFT;
             }
             //Else don't move horisontally
 
@@ -43,11 +45,13 @@ namespace KBS1.model
             if(playerY > Position_Y)
             {
                 Position_Y += Speed_Y;
+                Direction = ObjectDirection.DOWN;
             }
             //UP
             else if(playerY < Position_Y)
             {
                 Position_Y -= Speed_Y;
+                Direction = ObjectDirection.UP;
             }
             //Else don't move verticaly
         }
