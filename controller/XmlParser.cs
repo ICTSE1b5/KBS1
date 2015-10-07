19eb11f78df4f373f51b4b326c25311dde2fc2a8
Michiel-Daams <michiel-daams@hotmail.com>
2015-10-07 13:59:27 +0200

    Finished XmlParser for use

--- controller/XmlParser.cs
+++ controller/XmlParser.cs
@@ -36,40 +36,57 @@ namespace KBS1.controller
                             if(reader.Name!= "level" && reader.Name == "player")
                             {
                                 string hp = reader.GetAttribute("hp");
-                                string damage = reader.GetAttribute("damage");
-                                List<string> temp = new List<string> { reader.Name, hp, damage };
-                                this.data.Add(temp);
-                            }
-
-                            if (reader.Name != "level" && reader.Name == "start")
-                            {
-                                string hp = reader.GetAttribute("hp");
-                                string damage = reader.GetAttribute("damage");
-                                List<string> temp = new List<string> { reader.Name, hp, damage };
+                                string speed = reader.GetAttribute("speed");
+                                string x = reader.GetAttribute("x");
+                                string y = reader.GetAttribute("y");
+                                string width = reader.GetAttribute("width");
+                                string height = reader.GetAttribute("height");
+                                List<string> temp = new List<string> { reader.Name, hp, speed, x, y, width, height };
                                 this.data.Add(temp);
                             }
 
                             if (reader.Name != "level" && reader.Name == "finish")
                             {
-                                string hp = reader.GetAttribute("hp");
-                                string damage = reader.GetAttribute("damage");
-                                List<string> temp = new List<string> { reader.Name, hp, damage };
+                                string x = reader.GetAttribute("x");
+                                string y = reader.GetAttribute("y");
+                                string width = reader.GetAttribute("width");
+                                string height = reader.GetAttribute("height");
+                                List<string> temp = new List<string> { reader.Name, x, y, width, height };
                                 this.data.Add(temp);
                             }
 
                             if (reader.Name != "level" && reader.Name == "enemy")
                             {
                                 string hp = reader.GetAttribute("hp");
-                                string damage = reader.GetAttribute("damage");
-                                List<string> temp = new List<string> { reader.Name, hp, damage };
+                                string speed = reader.GetAttribute("speed");
+                                string x = reader.GetAttribute("x");
+                                string y = reader.GetAttribute("y");
+                                string width = reader.GetAttribute("width");
+                                string height = reader.GetAttribute("height");
+                                List<string> temp = new List<string> { reader.Name, hp, speed, x, y, width, height };
                                 this.data.Add(temp);
                             }
 
-                            if (reader.Name != "level" && reader.Name == "wall")
+                            if (reader.Name != "level" && reader.Name == "aura")
                             {
                                 string hp = reader.GetAttribute("hp");
-                                string damage = reader.GetAttribute("damage");
-                                List<string> temp = new List<string> { reader.Name, hp, damage };
+                                string speed = reader.GetAttribute("speed");
+                                string x = reader.GetAttribute("x");
+                                string y = reader.GetAttribute("y");
+                                string range = reader.GetAttribute("range");
+                                string width = reader.GetAttribute("width");
+                                string height = reader.GetAttribute("height");
+                                List<string> temp = new List<string> { reader.Name, hp, speed, x, y, range, width, height };
+                                this.data.Add(temp);
+                            }
+
+                            if (reader.Name != "level" && reader.Name == "static")
+                            {
+                                string x = reader.GetAttribute("x");
+                                string y = reader.GetAttribute("y");
+                                string width = reader.GetAttribute("width");
+                                string height = reader.GetAttribute("height");
+                                List<string> temp = new List<string> { reader.Name, x, y, width, height };
                                 this.data.Add(temp);
                             }
                             break;
