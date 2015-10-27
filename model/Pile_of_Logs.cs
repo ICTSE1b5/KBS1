b548eb1a810b3c77c9edbfab047f48e241cdf419
Michiel-Daams <michiel-daams@hotmail.com>
2015-10-27 12:17:56 +0100

    Edited descriptions on pile of logs and puddle

--- model/Pile_of_Logs.cs
+++ model/Pile_of_Logs.cs
@@ -13,7 +13,7 @@ namespace KBS1.model
             : base(pos_x, pos_y, 50, 50, 0, 0, 0, 999, 10, props, form)
         {
             this.image = Properties.Resources.log;
-            this.description = "This pile of logs will cause you to slow down so you don't trip on them.";
+            this.description = "This pile of logs will cause you to slow down.";
 
             isSolid = false;
         }
